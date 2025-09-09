9eac02eceef4164e662499f78334255c969ae218
ankitraj048 <ankit.raj@dianapps.com>
2025-09-09 16:48:08 +0530

    Add files via upload

---
+++ java/a.js
@@ -0,0 +1,19 @@
+console.log("ji")
+
+const obj = {
+  name : 'ankit',
+  age : 12
+}
+
+console.log(obj)  
+console.log(obj.age)  
+console.log(obj["name"])
+
+const myobj = {};
+myobj.name="ankit";
+myobj.age=12
+myobj.message = "hello brother"
+
+console.log(myobj)
+
+console.log(myobj.propertyName)
